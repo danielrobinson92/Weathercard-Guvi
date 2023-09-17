41b6e915d5f6743489e8d3bce24bc59596b8359e
danielrobinson92 <danielrobinson92@users.noreply.github.com>
2023-09-18 03:14:59 +0530

    Completed the Selection filter based on Continent & Country

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function weather(){
     let response = await fetch("https://restcountries.com/v3.1/all");
     let api = await response.json();
-    // console.log(api);
+    // console.log(api[0].name.common, api.length);
     return api;
 } 
 
@@ -10,38 +10,50 @@ async function cityWeather(){
     let api = await response.json();
     console.log(api);
 }
-// cityWeather();
 
 // Continent
 async function continent (){
-    console.log('test');
+    
     let response = await weather();
     let temp = response.map((x)=>{
         return x.continents.join("");       
     });
     let continent = temp.filter((x,i)=> temp.indexOf(x) == i);
     let htmlContinent = document.getElementById(`dropDown-continent`);
-
+    htmlContinent.innerHTML = `<option value="default">--Continent--</option>`;
     for (let i = 0; i < continent.length; i++){
         var x = document.createElement('option');
         x.innerHTML = `${continent[i]}`;
         x.setAttribute ("value",continent[i]);
         htmlContinent.append(x);
-        console.log(continent[i]);
     }
-
-    // Get the value of selected continent from the drop down
-    let selectedContinent = htmlContinent.addEventListener("change",function(){
-        console.log(htmlContinent.value);
+    // Get the value of selected continent from the drop down & filter the country
+    htmlContinent.addEventListener("change",function(){
+        let selectedContinent = htmlContinent.value;
+        let htmlCountry = document.getElementById(`dropDown-country`);
+        htmlCountry.innerHTML = `<option value="default">--Country--</option>`;
+            for (let j = 0; j < response.length; j++){
+                if (selectedContinent == response[j].continents[0]){
+                    var x = document.createElement('option');
+                    x.innerHTML = `${response[j].name.common}`;
+                    x.setAttribute ("value",response[j].name.common);
+                    htmlCountry.append(x);
+                    console.log(response[j].name.common, selectedContinent);
+                }
+        }
     });
-    console.log(selectedContinent);
 
+    htmlCountry.addEventListener("change",function(){
+        let selectedCountry = htmlCountry.value;
+        console.log(selectedCountry);
+    });
+    
 }
 
 
 // continent();
 
-
+weather();
 
 
 
